eb930b726e398c6beba90d4ac496174c179edbd3
Thisman <tablescable@gmail.com>
2018-11-13 15:36:46 +0300

    Scripting: base player controller

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+    public float Speed = 10f;
+
+    private Animator animator;
+
+    // Use this for initialization
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float translation = Input.GetAxis("Horizontal") * Speed;
+
+        translation *= Time.deltaTime;
+
+        if (translation > 0) {
+            animator.Play("person_walk");
+        }
+
+        if(translation < 0) {
+            animator.Play("person_walk_back");
+        }
+
+        transform.Translate(translation, 0, 0);
+    }
+}
